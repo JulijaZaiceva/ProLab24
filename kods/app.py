caaf31f955975a7c9db3f23f769a2c3c54260808
Jūlija Zaiceva <124576491+JulijaZaiceva@users.noreply.github.com>
2025-01-08 22:03:54 +0200

    Add files via upload

--- kods/app.py
+++ kods/app.py
@@ -1,3 +1,4 @@
+import uuid
 from datetime import datetime
 
 from flask import Flask, request, render_template, send_file, redirect, session, flash
@@ -9,7 +10,7 @@ import google.generativeai as genai
 from io import BytesIO
 import json
 from types import SimpleNamespace
-from database import init_db, save_to_db, fetch_from_db, save_user, find_user, import_emissions
+from database import init_db, save_to_db, fetch_from_db, save_user, find_user, import_emissions, create_bundle,fetch_user_bundles
 import xlsxwriter
 
 app = Flask(__name__)
@@ -18,7 +19,6 @@ app.config['DOWNLOAD_FOLDER'] = 'DataBase_downloaded'
 app.config['ALLOWED_EXTENSIONS'] = {'pdf'}
 app.secret_key = os.urandom(24)
 
-# Ваш API-ключ Google
 os.environ['mykey'] = "AIzaSyBJ2VMOr2zu6InzIz9E8pmx247eAJ6b2X4" 
 genai.configure(api_key=os.environ["mykey"])
 generation_config = {
@@ -34,37 +34,6 @@ model = genai.GenerativeModel(
 )
 init_db()
 list_of_dicts = import_emissions()
-type_id_to_co2 = {
-        1: 8.27,
-        2: 6.25,
-        3: 3.92,
-        4: 0.63,
-        6: 2.25,
-        7: 0.98,
-        8: 2.50,
-        9: 0.292,
-        10: 0.70,
-        11: 16.00,
-        12: 0.3,
-        13: 0.18,
-        14: 0.80,
-        15: 0.025,
-        16: 0.25,
-        17: 0.33,
-        18: 1.20,
-        19: 8.51,
-        20: 1.55,
-        21: 8.1,
-        22: 1.18,
-        23: 1.00,
-        24: 6.07,
-        25: 0.3,
-        26: 2.71,
-        27: 1166.67,
-        28: 6.73,
-        30: 0.4,
-        32: 0.309
-    }
 
 DATABASE = "invoices.db"
 
@@ -76,7 +45,7 @@ def index():
     if 'user_id' not in session:
         return redirect('/login')
     
-    return render_template('index.html', invoices= fetch_from_db(), message=None)
+    return render_template('index.html', extracted_data= fetch_from_db(session.get('bundle_id')), message=None)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -86,6 +55,11 @@ def upload_file():
     files = request.files.getlist('files')
     all_data = []
 
+    print(session.get("bundle_id"))
+    if session.get("bundle_id") is None:
+        session["bundle_id"] = str(uuid.uuid4())
+
+    print(session.get("bundle_id"))
     jsonData = "[{\"typeid\":1,\"Type_Name\":\"FOOD\",\"Type_Decode_LV\":\"Pārtikas izstrādājumi\",\"Mērvienība\":\"kg / vienība\"},{\"typeid\":2,\"Type_Name\":\"CHOC\",\"Type_Decode_LV\":\"Šokolādes izstrādājumi\",\"Mērvienība\":\"kg\"},{\"typeid\":3,\"Type_Name\":\"TEL\",\"Type_Decode_LV\":\"Komunikācijas pakalpojumi\",\"Mērvienība\":\"mēn. / vienība\"},{\"typeid\":4,\"Type_Name\":\"TOOL\",\"Type_Decode_LV\":\"Instrumenti\",\"Mērvienība\":\"vienības\"},{\"typeid\":6,\"Type_Name\":\"GAS\",\"Type_Decode_LV\":\"Gāze\",\"Mērvienība\":\"l\"},{\"typeid\":7,\"Type_Name\":\"ELEC\",\"Type_Decode_LV\":\"Elektrība\",\"Mērvienība\":\"kWh/vienība\"},{\"typeid\":8,\"Type_Name\":\"FUEL\",\"Type_Decode_LV\":\"Degviela\",\"Mērvienība\":\"l\"},{\"typeid\":9,\"Type_Name\":\"OFF\",\"Type_Decode_LV\":\"Kancelejas preces\",\"Mērvienība\":\"vienība\"},{\"typeid\":10,\"Type_Name\":\"BEER\",\"Type_Decode_LV\":\"Alus\",\"Mērvienība\":\"l\"},{\"typeid\":11,\"Type_Name\":\"COSM\",\"Type_Decode_LV\":\"Kosmetikas piederumi\",\"Mērvienība\":\"vienība\"},{\"typeid\":12,\"Type_Name\":\"TRAV\",\"Type_Decode_LV\":\"Ceļojumi\",\"Mērvienība\":\"vienība\"},{\"typeid\":13,\"Type_Name\":\"POST\",\"Type_Decode_LV\":\"Pasta pakalpojumi\",\"Mērvienība\":\"vienība\"},{\"typeid\":14,\"Type_Name\":\"DAIRY\",\"Type_Decode_LV\":\"Piena pārtika\",\"Mērvienība\":\"kg/l\"},{\"typeid\":15,\"Type_Name\":\"WOOD\",\"Type_Decode_LV\":\"Koka izstrādājumi\",\"Mērvienība\":\"m3\"},{\"typeid\":16,\"Type_Name\":\"PHAR\",\"Type_Decode_LV\":\"Medikamenti\",\"Mērvienība\":\"vienība\"},{\"typeid\":17,\"Type_Name\":\"GLASS\",\"Type_Decode_LV\":\"Stikla izstrādājumi\",\"Mērvienība\":\"kg\"},{\"typeid\":18,\"Type_Name\":\"ENT\",\"Type_Decode_LV\":\"Ūdens izklaides pakalpojumi\",\"Mērvienība\":\"vienība\"},{\"typeid\":19,\"Type_Name\":\"CLOTH\",\"Type_Decode_LV\":\"Tekstīla izstrādājumi\",\"Mērvienība\":\"vienība\"},{\"typeid\":20,\"Type_Name\":\"ALCO\",\"Type_Decode_LV\":\"Alkohola dzērieni\",\"Mērvienība\":\"l/vienība\"},{\"typeid\":21,\"Type_Name\":\"CONST\",\"Type_Decode_LV\":\"Celtniecības pakalpojumi\",\"Mērvienība\":\"t/vienība\"},{\"typeid\":22,\"Type_Name\":\"TRANS\",\"Type_Decode_LV\":\"Transportēšanas pakalpojumi\",\"Mērvienība\":\"l/t/vienība\"},{\"typeid\":23,\"Type_Name\":\"RETAIL\",\"Type_Decode_LV\":\"Pārdošanas pakalpojumi\",\"Mērvienība\":\"vienība\"},{\"typeid\":24,\"Type_Name\":\"METAL\",\"Type_Decode_LV\":\"Metalurģijas izstrādājumi\",\"Mērvienība\":\"t/vienība\"},{\"typeid\":25,\"Type_Name\":\"HOME\",\"Type_Decode_LV\":\"Mājas apsaimniekošanas pakalpojumi\",\"Mērvienība\":\"mēn\"},{\"typeid\":26,\"Type_Name\":\"OIL\",\"Type_Decode_LV\":\"Naftas izstrādājumi\",\"Mērvienība\":\"l\"},{\"typeid\":27,\"Type_Name\":\"RECYCLE\",\"Type_Decode_LV\":\"Pārstrādes pakalpojumi\",\"Mērvienība\":\"t\"},{\"typeid\":28,\"Type_Name\":\"INS\",\"Type_Decode_LV\":\"Apdrošināšanas pakalpojumi\",\"Mērvienība\":\"vienība\"},{\"typeid\":30,\"Type_Name\":\"TICKET\",\"Type_Decode_LV\":\"Izklaides pakalpojumi\",\"Mērvienība\":\"vienība\"},{\"typeid\":32,\"Type_Name\":\"HEAT\",\"Type_Decode_LV\":\"Apkures pakalpojumi\",\"Mērvienība\":\"kWh\"}]"
 
     for file in files:
@@ -94,7 +68,6 @@ def upload_file():
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(filepath)
 
-            # Вызов Google API для обработки данных
             with open(filepath, "rb") as file_content:
                 myfile = genai.upload_file(filepath)
                 result = model.generate_content([ 
@@ -124,15 +97,10 @@ def upload_file():
                     "\n"
                 ])
             outputJson = result.text.replace("json", "").replace("`", "").replace("\t", "").replace("\n", "")
-            print(outputJson)
+
             output = json.loads(outputJson, object_hook=lambda d: SimpleNamespace(**d))
             for itemInOutput in output:
-                print()
-                print(list_of_dicts)
-                print(itemInOutput)
-                print()
                 itemEmissionByUnit = [item for item in list_of_dicts if item['type_id'] == itemInOutput.typeid][0]['value']
-                print(itemEmissionByUnit)
                 itemEmission = itemEmissionByUnit * itemInOutput.correctQuantity
                 all_data.append((
                                 itemInOutput.company,
@@ -141,27 +109,31 @@ def upload_file():
                                 itemInOutput.quantity,
                                 itemInOutput.price,
                                 itemEmission,
-                                filename
+                                filename,
+                                session['bundle_id']
                             ))
 
     if all_data:
         save_to_db(all_data)
 
-    return render_template('index.html', message="Dati veiksmīgi saglabāti!", extracted_data=all_data)
+    return redirect('/')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
-
-        try:
-            save_user(username, generate_password_hash(password))
-            flash('Registration successful!', 'success')
-            return redirect('/login')
-        except sqlite3.IntegrityError:
-            flash('Username already exists!', 'error')
+        password_repeat = request.form['passwordtwo']
+        if password_repeat == password:
+            try:
+                save_user(username, generate_password_hash(password))
+                flash('Registration successful!', 'success')
+                return redirect('/login')
+            except sqlite3.IntegrityError:
+                flash('Username already exists!', 'error')
+        else:
+            flash('Passwords does not match!', 'error')
 
     return render_template('register.html')
 
@@ -170,11 +142,9 @@ def profile():
     if 'user_id' not in session:
         return redirect('/login')
 
-    bundles = []
-    bundles.append(("first","second","third"))
-    bundles.append(("fourth","fifth","sixth"))
-
-    return render_template('profile.html', bundles = bundles)
+    bundles = fetch_user_bundles(session.get("username"))
+    print(bundles)
+    return render_template('profile.html', extracted_data = bundles)
 
 @app.route('/logout')
 def logout():
@@ -194,6 +164,7 @@ def login():
             session['user_id'] = user[0]
             session['username'] = username
             session['role'] = user[2]
+            session['bundle_id'] = None
             flash('Login successful!', 'success')
             return redirect('/')
         else:
@@ -206,13 +177,16 @@ def export_to_excel():
     if 'user_id' not in session:
         return redirect('/login')
     try:
-        # Убедитесь, что папка для скачивания существует
+        bundle_id_from_url = request.args.get("bundle_id")
+        if bundle_id_from_url is not None:
+            session['bundle_id'] = bundle_id_from_url
+
         if not os.path.exists(app.config['DOWNLOAD_FOLDER']):
             os.makedirs(app.config['DOWNLOAD_FOLDER'])
 
-        # Считывание данных из базы данных
         with sqlite3.connect(DATABASE) as conn:
-            df = pd.read_sql_query("SELECT * FROM invoices", conn)
+            df = pd.read_sql_query("SELECT * FROM invoices where bundle_id = ?", conn, params=(session.get("bundle_id"),))
+
 
         output = BytesIO()
         with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
@@ -221,15 +195,12 @@ def export_to_excel():
             workbook = writer.book
             worksheet = writer.sheets['Invoices']
 
-            # Identify the column with values to sum (e.g., 'amount')
             total_column = 'emisija'
             if total_column in df.columns:
-                # Get the column index (0-based) for the 'amount' column
                 col_idx = df.columns.get_loc(total_column)
-                # Calculate the range for summation
-                start_row = 1  # Row 1 in Excel corresponds to DataFrame index 0
-                end_row = len(df)  # Length of the DataFrame
-                total_row = end_row + 1  # Total row comes after the last data row
+                start_row = 1
+                end_row = len(df)
+                total_row = end_row + 1
 
                 formula = f"=SUM({chr(65 + col_idx)}{start_row + 1}:{chr(65 + col_idx)}{end_row + 1})"
                 worksheet.write(total_row, col_idx, formula, workbook.add_format({'bold': True}))
@@ -238,6 +209,9 @@ def export_to_excel():
 
         output.seek(0)
         today = datetime.today().strftime('%d/%m/%Y').replace("/","-")
+        if bundle_id_from_url is None:
+            create_bundle(session['bundle_id'], session['username'])
+        session['bundle_id'] = None
         return send_file(
             output,
             mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
@@ -249,7 +223,6 @@ def export_to_excel():
         return render_template("index.html", message="Kļūda, mēģiniet vēlreiz!", show_popup="True")
 
 if __name__ == '__main__':
-    # Инициализация базы данных
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
     if not os.path.exists(app.config['DOWNLOAD_FOLDER']):
--- kods/database.py
+++ kods/database.py
@@ -9,11 +9,15 @@ def init_db():
         cursor = conn.cursor()
 
         cursor.execute("""
-                DROP TABLE invoices
+                DROP TABLE IF EXISTS invoices
                 """)
 
         cursor.execute("""
-                        DROP TABLE users
+                        DROP TABLE IF EXISTS users
+                        """)
+
+        cursor.execute("""
+                        DROP TABLE IF EXISTS bundles
                         """)
 
         cursor.execute("""
@@ -36,12 +40,77 @@ def init_db():
             daudzums TEXT,
             cena TEXT,
             emisija FLOAT,
-            fails TEXT
+            fails TEXT,
+            bundle_id TEXT
         )
         """)
-        conn.commit()
-    # import_emissions()
 
+        cursor.execute("""
+                CREATE TABLE IF NOT EXISTS bundles (
+                    id TEXT PRIMARY KEY,
+                    username TEXT,
+                    Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+                )
+                """)
+
+        conn.commit()
+
+def fetch_user_bundles(username):
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT 
+                b.id AS bundle_id,
+                b.timestamp AS bundle_timestamp,
+                i.id AS invoice_id,
+                i.firma,
+                i.datums,
+                i.produkts,
+                i.daudzums,
+                i.cena,
+                i.emisija,
+                i.fails
+            FROM bundles b
+            LEFT JOIN invoices i ON b.id = i.bundle_id
+            WHERE b.username = ?
+            ORDER BY b.timestamp ASC, i.id ASC
+        """, (username,))
+
+        rows = cursor.fetchall()
+        bundles = {}
+
+        for row in rows:
+            bundle_id = row[0]
+            bundle_timestamp = row[1]
+            invoice_data = {
+                "invoice_id": row[2],
+                "firma": row[3],
+                "datums": row[4],
+                "produkts": row[5],
+                "daudzums": row[6],
+                "cena": row[7],
+                "emisija": row[8],
+                "fails": row[9]
+            }
+
+            if bundle_id not in bundles:
+                bundles[bundle_id] = {
+                    "bundle_id": bundle_id,
+                    "timestamp": bundle_timestamp,
+                    "invoices": []
+                }
+            if invoice_data["invoice_id"]:
+                bundles[bundle_id]["invoices"].append(invoice_data)
+
+        return list(bundles.values())
+
+def create_bundle(id, username):
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO bundles (id, username) VALUES (?, ?)",
+                       (id, username))
+        conn.commit()
 
 def import_emissions():
     df = pd.read_excel(EXCEL_FILE, sheet_name=1)
@@ -54,42 +123,23 @@ def import_emissions():
     })
     mapped_df = mapped_df[["type_id", "type_name","type_decode_lv","units","value"]]
     return mapped_df.to_dict(orient="records")
-    # with sqlite3.connect(DATABASE) as conn:
-    #     cursor = conn.cursor()
-    #     cursor.execute("""
-    #                     DROP TABLE IF EXISTS emissions
-    #                     """)
-    #
-    #     cursor.execute("""
-    #     CREATE TABLE IF NOT EXISTS emissions (
-    #         type_id INTEGER PRIMARY KEY,
-    #         type_name TEXT,
-    #         type_decode_lv TEXT,
-    #         units TEXT,
-    #         value FLOAT
-    #     )
-    #     """)
-
-        # mapped_df.to_sql('emissions', conn, if_exists='append', index=False)
-        # conn.commit()
-
 
 def save_to_db(data):
-    print()
-    print(data)
-    print()
     with sqlite3.connect(DATABASE) as conn:
         cursor = conn.cursor()
         cursor.executemany("""
-        INSERT INTO invoices (firma, datums, produkts, daudzums, cena, emisija, fails)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO invoices (firma, datums, produkts, daudzums, cena, emisija, fails, bundle_id)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         """, data)
         conn.commit()
 
-def fetch_from_db():
+def fetch_from_db(bundle_id):
+    if not bundle_id:
+        return []
+
     with sqlite3.connect(DATABASE) as conn:
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM invoices")
+        cursor.execute("SELECT firma, datums, produkts, daudzums, cena, emisija, fails FROM invoices where bundle_id = ?", (bundle_id,))
         return cursor.fetchall()
 
 def save_user(username, hashed_password):
